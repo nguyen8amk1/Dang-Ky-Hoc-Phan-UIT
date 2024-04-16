337c274cf4ff8cdb89f900fdbd3d6ca3d1fb8527
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-04-16 17:49:45 +0700

    change certain stuff

--- prod-compose.sh
+++ prod-compose.sh
@@ -1,4 +1,4 @@
-if ! sudo docker-compose --file ./prod-docker-compose.yml up; then
+if ! sudo docker compose --file ./prod-docker-compose.yml up; then
 	echo "Error: Something wrong with docker compose"
 	exit 0
 fi
