b03bff84160248c015c6f94c1bd9ec830dcbc7fb
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-05-23 18:34:10 +0700

    refactor: organize the state names a little bit

--- client/src/pages/LichThi_GoogleCalendarGenerator.js
+++ client/src/pages/LichThi_GoogleCalendarGenerator.js
@@ -2,42 +2,57 @@ import SubmitYourHocPhanInfo from '../components/SubmitYourHocPhanInfo';
 import GeneratedLichThi from '../components/GeneratedLichThi';
 import {useState, useEffect} from 'react';
 
+const States = {
+    CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE: 0, 
+    SUBMIT_TTDKHP: 1,
+    SHOW_CALENDAR_PREVIEW: 2, 
+};
+
 export function LichThi_GoogleCalendarGenerator() {
     const [goodSubmittedInfoEvent, setGoodSubmittedInfoEvent] = useState(false);
     const [wantToUploadAnotherTTDKHPEvent, setWantToUploadAnotherTTDKHPEvent] = useState(false);    
-    const [currentState, setCurrentState] = useState("CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE");
+    //const [currentState, setCurrentState] = useState("CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE");
+    const [currentState, setCurrentState] = useState(States.CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE);
 
     useEffect(() => {
         console.log("Re-render")
         switch(currentState) {
-            case "CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE": {
+            //case "CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE": {
+            case States.CHECK_IF_LICH_THI_HAVE_BEEN_GENERATED_BEFORE: {
                 const lichThiHaveBeenGenerated = localStorage.getItem("raw-lichthi-schedule") !== null; 
                 if(lichThiHaveBeenGenerated == true) {
-                    setCurrentState("SHOW_CALENDAR_PREVIEW");
+                    //setCurrentState("SHOW_CALENDAR_PREVIEW");
+                    setCurrentState(States.SHOW_CALENDAR_PREVIEW);
                 } else {
-                    setCurrentState("SUBMIT_TTDKHP");
+                    //setCurrentState("SUBMIT_TTDKHP");
+                    setCurrentState(States.SUBMIT_TTDKHP);
                 }
 
                 break;
             }
-            case "SUBMIT_TTDKHP": {
+            //case "SUBMIT_TTDKHP": {
+            case States.SUBMIT_TTDKHP: {
                 if(goodSubmittedInfoEvent) {
                     setGoodSubmittedInfoEvent(false); // Reset the event  
 
-                    setCurrentState("SHOW_CALENDAR_PREVIEW");
+                    //setCurrentState("SHOW_CALENDAR_PREVIEW");
+                    setCurrentState(States.SHOW_CALENDAR_PREVIEW);
                 } else {
-                    setCurrentState("SUBMIT_TTDKHP");
+                    //setCurrentState("SUBMIT_TTDKHP");
+                    setCurrentState(States.SUBMIT_TTDKHP);
                 }
 
                 break;
             } 
-            case 'SHOW_CALENDAR_PREVIEW': {
+            //case 'SHOW_CALENDAR_PREVIEW': {
+            case States.SHOW_CALENDAR_PREVIEW: {
                 if(wantToUploadAnotherTTDKHPEvent) {
                     setWantToUploadAnotherTTDKHPEvent(false); // Reset the event 
 
 
 
-                    setCurrentState("SUBMIT_TTDKHP");
+                    //setCurrentState("SUBMIT_TTDKHP");
+                    setCurrentState(States.SUBMIT_TTDKHP);
                 }
             }
         }
@@ -45,11 +60,11 @@ export function LichThi_GoogleCalendarGenerator() {
 
     return (
         <>
-            {(  currentState==="SUBMIT_TTDKHP") && 
+            {(  currentState===States.SUBMIT_TTDKHP) && 
                 <SubmitYourHocPhanInfo setGoodSubmittedInfoEvent={setGoodSubmittedInfoEvent}/>
             }
 
-            {(  currentState==="SHOW_CALENDAR_PREVIEW") && 
+            {(  currentState===States.SHOW_CALENDAR_PREVIEW) && 
                 <GeneratedLichThi setWantToUploadAnotherTTDKHPEvent={setWantToUploadAnotherTTDKHPEvent}/>
             } 
         </>
